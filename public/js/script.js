a211603530361139e2155edd503cd149d8768a92
Arlefreak <arlefreak@gmail.com>
2016-03-04 14:19:12 -0600

    Fixed font on mobile

--- public/js/script.js
+++ public/js/script.js
@@ -13,6 +13,178 @@ module.exports = {
 var i = 1;
 console.log(i);
 
+import fetch from 'isomorphic-fetch';
+import constants from '../constants.js';
+const apiURL = constants.APIURL;
+
+export const SET_CATEGORY_FILTER = 'SET_CATEGORY_FILTER';
+export function setCategoryFilter(id, name) {
+    return {
+        type: SET_CATEGORY_FILTER,
+        id,
+        name
+    };
+}
+
+export const ADD_TAG_FILTER = 'ADD_TAG_FILTER';
+export const DELETE_TAG_FILTER = 'DELETE_TAG_FILTER';
+export const CLEAR_ALL_TAG_FILTERS = 'CLEAR_ALL_TAG_FILTERS';
+
+export function addTagFilter(id, name) {
+    return { 
+        type: ADD_TAG_FILTER,
+        id,
+        name
+    };
+}
+
+export function deleteTagFilter(id) {
+    return {
+        type: DELETE_TAG_FILTER,
+        id
+    };
+}
+
+export function clearTagFilter() {
+    return {
+        type: CLEAR_ALL_TAG_FILTERS
+    };
+}
+
+export const API_REQUEST = 'API_REQUEST';
+function apiRequest(endPoint) {
+    return {
+        type: API_REQUEST,
+        endPoint
+    };
+}
+
+
+export const API_RESPONSE = 'API_RESPONSE';
+export function apiResponse(json, endPoint) {
+    return {
+        type: API_RESPONSE,
+        endPoint,
+        projects: json,
+        receivedAt: Date.now()
+    };
+}
+
+export function apiFetch(endPoint) {
+    return function (dispatch) {
+        dispatch(apiRequest(endPoint));
+        return fetch(apiURL + endPoint)
+        .then(response => response.json())
+        .then(json =>
+              dispatch(apiResponse(json, endPoint)),
+              dispatch(filterProjects())
+             );
+    };
+}
+
+export function apiFetchIfNeeded(endPoint){
+    // Note that the function also receives getState()
+    // which lets you choose what to dispatch next.
+
+    // This is useful for avoiding a network request if
+    // a cached value is already available.
+
+    return (dispatch, getState) => {
+        if (apiShouldFetch(getState(), endPoint)) {
+            // Dispatch a thunk from thunk!
+            return dispatch(apiFetch(endPoint));
+        } else {
+            // Let the calling code know there's nothing to wait for.
+            return Promise.resolve();
+        }
+    };
+}
+
+function apiShouldFetch(state, endPoint) {
+    const apiCall = state.apiCalls[endPoint];
+    if (!apiCall) {
+        return true;
+    } else if (apiCall.isFetching) {
+        return false;
+    } else {
+        return false;
+    }
+}
+
+export const SET_VISIBLE_PROJECTS = 'SET_VISIBLE_PROJECTS';
+export function setVisibleProjects(projects) {
+    const PROJECTS = projects || [];
+    return {
+        type: SET_VISIBLE_PROJECTS,
+        projects: projects
+    };
+}
+
+export function filterProjects() {
+    return function (dispatch, getState){
+        const state = getState() || {};
+        const apiCalls = state['apiCalls'] || [];
+        const projects = apiCalls['projects'] || {};
+        const items = projects.items || [];
+        const categoryFilter = state['categoryFilter'];
+        const tagFilter = state['tagFilter'] || [];
+        let filterProjects = filterByCategory(items, categoryFilter);
+        filterProjects = filterByTags(filterProjects, tagFilter);
+        dispatch(setVisibleProjects(filterProjects));
+    };
+}
+
+function filterByCategory (projects, category) {
+    if(projects.length > 0){
+        if(category.id === 0){
+            return projects;
+        }else{
+            return projects.filter(t => t.category === category.id);
+        }
+    }else{
+        return projects;
+    }
+};
+
+function filterByTags (projects, tags) {
+    var filteredProjects = [];
+    var i = 0;
+    var j = 0;
+    var k = 0;
+    var project;
+    if(projects && tags){
+        if(projects.length > 0 && tags.length > 0){
+            for(i; i < projects.length; i++){
+                project = projects[i];
+                j = 0;
+                if(project){
+                    for(j; j < project.tags.length; j++){
+                        var b = false;
+                        k = 0;
+                        for(k; k < tags.length; k++){
+                            if(project.tags[j].id === tags[k].id){
+                                filteredProjects.push(project);
+                                b = true;
+                                break;
+                            }
+                        }
+                        if(b){
+                            break;
+                        }
+                    }
+                }
+            }
+        }else{
+            filteredProjects = projects;
+        }
+    }else{
+        filteredProjects = projects;
+    }
+    return filteredProjects;
+};
+
+
+
 import { connect } from 'react-redux';
 import  CategoryRow from '../components/categoryRow.jsx';;
 
@@ -541,175 +713,3 @@ let store =  applyMiddleware(
 store.dispatch(fetchPosts());
 store.dispatch(addTagFilter(1, 'phaser'));
 store.dispatch(setCategoryFilter(1, 'Games'));
-
-import fetch from 'isomorphic-fetch';
-import constants from '../constants.js';
-const apiURL = constants.APIURL;
-
-export const SET_CATEGORY_FILTER = 'SET_CATEGORY_FILTER';
-export function setCategoryFilter(id, name) {
-    return {
-        type: SET_CATEGORY_FILTER,
-        id,
-        name
-    };
-}
-
-export const ADD_TAG_FILTER = 'ADD_TAG_FILTER';
-export const DELETE_TAG_FILTER = 'DELETE_TAG_FILTER';
-export const CLEAR_ALL_TAG_FILTERS = 'CLEAR_ALL_TAG_FILTERS';
-
-export function addTagFilter(id, name) {
-    return { 
-        type: ADD_TAG_FILTER,
-        id,
-        name
-    };
-}
-
-export function deleteTagFilter(id) {
-    return {
-        type: DELETE_TAG_FILTER,
-        id
-    };
-}
-
-export function clearTagFilter() {
-    return {
-        type: CLEAR_ALL_TAG_FILTERS
-    };
-}
-
-export const API_REQUEST = 'API_REQUEST';
-function apiRequest(endPoint) {
-    return {
-        type: API_REQUEST,
-        endPoint
-    };
-}
-
-
-export const API_RESPONSE = 'API_RESPONSE';
-export function apiResponse(json, endPoint) {
-    return {
-        type: API_RESPONSE,
-        endPoint,
-        projects: json,
-        receivedAt: Date.now()
-    };
-}
-
-export function apiFetch(endPoint) {
-    return function (dispatch) {
-        dispatch(apiRequest(endPoint));
-        return fetch(apiURL + endPoint)
-        .then(response => response.json())
-        .then(json =>
-              dispatch(apiResponse(json, endPoint)),
-              dispatch(filterProjects())
-             );
-    };
-}
-
-export function apiFetchIfNeeded(endPoint){
-    // Note that the function also receives getState()
-    // which lets you choose what to dispatch next.
-
-    // This is useful for avoiding a network request if
-    // a cached value is already available.
-
-    return (dispatch, getState) => {
-        if (apiShouldFetch(getState(), endPoint)) {
-            // Dispatch a thunk from thunk!
-            return dispatch(apiFetch(endPoint));
-        } else {
-            // Let the calling code know there's nothing to wait for.
-            return Promise.resolve();
-        }
-    };
-}
-
-function apiShouldFetch(state, endPoint) {
-    const apiCall = state.apiCalls[endPoint];
-    if (!apiCall) {
-        return true;
-    } else if (apiCall.isFetching) {
-        return false;
-    } else {
-        return false;
-    }
-}
-
-export const SET_VISIBLE_PROJECTS = 'SET_VISIBLE_PROJECTS';
-export function setVisibleProjects(projects) {
-    const PROJECTS = projects || [];
-    return {
-        type: SET_VISIBLE_PROJECTS,
-        projects: projects
-    };
-}
-
-export function filterProjects() {
-    return function (dispatch, getState){
-        const state = getState() || {};
-        const apiCalls = state['apiCalls'] || [];
-        const projects = apiCalls['projects'] || {};
-        const items = projects.items || [];
-        const categoryFilter = state['categoryFilter'];
-        const tagFilter = state['tagFilter'] || [];
-        let filterProjects = filterByCategory(items, categoryFilter);
-        filterProjects = filterByTags(filterProjects, tagFilter);
-        dispatch(setVisibleProjects(filterProjects));
-    };
-}
-
-function filterByCategory (projects, category) {
-    if(projects.length > 0){
-        if(category.id === 0){
-            return projects;
-        }else{
-            return projects.filter(t => t.category === category.id);
-        }
-    }else{
-        return projects;
-    }
-};
-
-function filterByTags (projects, tags) {
-    var filteredProjects = [];
-    var i = 0;
-    var j = 0;
-    var k = 0;
-    var project;
-    if(projects && tags){
-        if(projects.length > 0 && tags.length > 0){
-            for(i; i < projects.length; i++){
-                project = projects[i];
-                j = 0;
-                if(project){
-                    for(j; j < project.tags.length; j++){
-                        var b = false;
-                        k = 0;
-                        for(k; k < tags.length; k++){
-                            if(project.tags[j].id === tags[k].id){
-                                filteredProjects.push(project);
-                                b = true;
-                                break;
-                            }
-                        }
-                        if(b){
-                            break;
-                        }
-                    }
-                }
-            }
-        }else{
-            filteredProjects = projects;
-        }
-    }else{
-        filteredProjects = projects;
-    }
-    return filteredProjects;
-};
-
-
