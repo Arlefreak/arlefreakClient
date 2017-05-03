68eb06c244e0f8cf1e7536af76f42e6056dd0cc1
Arlefreak <hi@arlefreak.com>
2017-05-02 22:47:01 -0500

    Now it config from server

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,41 @@
 const https = require('https');
+const URL = 'https://api.ellugar.co/web_client/config/';
 
 exports.index = (req, res)=>{
     let date = new Date(Date.now());
     let year = date.getFullYear();
-
-    res.render('home', {
-        title: 'ellugar.co',
-        description: 'A place with the shape of air, the habitat for work, experiments and creativity.',
+    let context = {
+        title: '',
+        description: '',
         year: year,
-        preview: 'preview.png',
+        preview: '',
+    };
+
+    https.get(URL, (_res) => {
+        _res.on('data', (d) => {
+            let context = {
+                title: '',
+                description: '',
+                year: year,
+                preview: '',
+            };
+
+            let data = JSON.parse(d);
+            context.title = data.name;
+            context.description = data.default_description;
+            context.preview = data.default_preview;
+            res.render('home', context);
+        });
+
+    }).on('error', (e) => {
+        let context = {
+            title: '',
+            description: '',
+            year: year,
+            preview: '',
+        };
+        console.error(e);
+        res.render('home', context);
     });
+
 };
