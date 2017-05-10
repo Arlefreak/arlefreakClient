09b64e6022969b84b017a9f219bd52bb3fd0fed9
Arlefreak <hi@arlefreak.com>
2017-05-10 01:39:38 -0500

    Reverte home

--- src/js/components/home.jsx
+++ src/js/components/home.jsx
@@ -16,9 +16,9 @@ const Container = ({ id, title, isFetching, items, images, config, route }) => {
         title = { title }
         isFetching = { isFetching }
     >
-        { config != null && <div className="markdown no-margin" dangerouslySetInnerHTML={{ __html: mdr }}/> }
-        { images != null && <div className="margin"></div> }
         <Soon></Soon>
+        { config != null && <div className="markdown" dangerouslySetInnerHTML={{ __html: mdr }}/> }
+        { images != null && <div className="margin"></div> }
         { images != null && <ImageList className="full-width" images={ images } items={ items }></ImageList> }
     </Page>
     );
