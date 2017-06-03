dbcf545d84f475b20ef70f532043c16094e227ab
Arlefreak <hi@arlefreak.com>
2017-06-03 12:12:57 -0500

    Fixed header and changed to tiny letter

--- src/js/components/subscribe.jsx
+++ src/js/components/subscribe.jsx
@@ -3,18 +3,17 @@ import PropTypes from 'prop-types';
 
 const Container = () => {
     return (
-        <div id="mc_embed_signup" className="subscribe-form">
-            <form action="//ellugar.us10.list-manage.com/subscribe/post?u=3d6bfe919b1589eeacd4b646e&amp;id=0d53f511b1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
-                <input type="email" name="EMAIL" className="required email" placeholder="email" id="mce-EMAIL"/>
-                <div 
-                    className="hidden"
-                    aria-hidden="true">
-                    <input type="text" name="b_3d6bfe919b1589eeacd4b646e_0d53f511b1" tabIndex="-1" value="" />
-                </div>
-                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
-            </form>
-        </div>
+        <div className="subscribe-form">
+        <form  action="https://tinyletter.com/afk" method="post" target="popupwindow" onSubmit="window.open('https://tinyletter.com/afk', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
+            <input type="email" name="email" className="required email" placeholder="email" id="tlemail"/>
+            <input type="hidden" value="1" name="embed"/>
+            <input type="submit" value="Subscribe" className="button" />
+        </form>
+    </div>
     );
 };
 
 export default Container;
+
+
+
