12b1c02f5b8bc2e8eb7b43d9ab415579f32eb081
afk <afk@ellugar.co>
2018-01-03 21:03:10 -0600

    Added download button to podcast episode view

--- src/js/components/episode.jsx
+++ src/js/components/episode.jsx
@@ -5,75 +5,123 @@ import Loading from './loading.jsx';
 import Remarkable from 'remarkable';
 import AudioPlayer from '../containers/audio__player.js';
 
-const Episode = ({ 
-    item,
-    slug,
-    iTunesURL,
-    feed,
-    prev,
-    next,
-}) => {
-    var md = new Remarkable();
-    var text = item.text;
-    var mdr = md.render(text);
-    return(
-        <section className="episode">
-            { item.dateCreated != null && 
-                    <div className="date-container">
-                        <span className="date">{ item.dateCreated } |</span> 
-                        <span className="date"> { item.dateUpdated }</span>
-                    </div>
-            }
-            <div className="episode-info">
-                <div className="episode-cover" >
-                    <img
-                        src={ item.image }
-                        alt={ item.title }
-                    />
-                </div>
-                <div className="podcast-column episode-buttons">
-                    <ul className="subscribe-buttons">
-                        { prev != null &&
-                                <li><Link className="button yellow" to={ `/podcasts/${slug}/${prev}` }>prev</Link></li>
-                        }
-                        { prev == null &&
-                                <li><Link className="button yellow" to={ `/podcasts/${slug}/` }>all</Link></li>
-                        }
-                        { next != null &&
-                                <li><Link className="button yellow" to={ `/podcasts/${slug}/${next}` }>next</Link></li>
-                        }
-                        { next == null &&
-                                <li><Link className="button yellow" to={ `/podcasts/${slug}/` }>all</Link></li>
-                        }
-                        <li><a rel="noopener noreferrer" target="_blank" className="button pink" href={ feed }>rss</a></li>
-                        { iTunesURL != null &&
-                                <li><a rel="noopener noreferrer" target="_blank" className="button blue" href={ iTunesURL }>iTunes</a></li>
-                        }
-                    </ul>
-                </div>
-                <div className="margin"></div>
-                <AudioPlayer url_mp3={ item.audio_ogg } url_ogg={ item.audio_mp3 } duration={ item.duration } />
-            </div>
+const Episode = ({ item, slug, iTunesURL, feed, prev, next }) => {
+  var md = new Remarkable();
+  var text = item.text;
+  var mdr = md.render(text);
+  return (
+    <section className="episode">
+      {item.dateCreated != null && (
+        <div className="date-container">
+          <span className="date">{item.dateCreated} |</span>
+          <span className="date"> {item.dateUpdated}</span>
+        </div>
+      )}
+      <div className="episode-info">
+        <div className="episode-cover">
+          <img src={item.image} alt={item.title} />
+        </div>
+        <div className="podcast-column episode-buttons">
+          <ul className="subscribe-buttons">
+            {prev != null && (
+              <li>
+                <Link
+                  className="button yellow"
+                  to={`/podcasts/${slug}/${prev}`}
+                >
+                  prev
+                </Link>
+              </li>
+            )}
+            {prev == null && (
+              <li>
+                <Link className="button yellow" to={`/podcasts/${slug}/`}>
+                  all
+                </Link>
+              </li>
+            )}
+            {next != null && (
+              <li>
+                <Link
+                  className="button yellow"
+                  to={`/podcasts/${slug}/${next}`}
+                >
+                  next
+                </Link>
+              </li>
+            )}
+            {next == null && (
+              <li>
+                <Link className="button yellow" to={`/podcasts/${slug}/`}>
+                  all
+                </Link>
+              </li>
+            )}
+            <li>
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                className="button pink"
+                href={feed}
+              >
+                rss
+              </a>
+            </li>
+            {iTunesURL != null && (
+              <li>
+                <a
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="button blue"
+                  href={iTunesURL}
+                >
+                  iTunes
+                </a>
+              </li>
+            )}
+            <li>
+              <a
+                className="button yellow"
+                href={item.audio_ogg}
+                rel="noopener noreferrer"
+                target="_blank"
+                downlod={`${item.title}.ogg`}
+              >
+                download
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="margin" />
+        <AudioPlayer
+          url_mp3={item.audio_ogg}
+          url_ogg={item.audio_mp3}
+          duration={item.duration}
+        />
+      </div>
 
-            <div className="episode-description markdown" dangerouslySetInnerHTML={{ __html: mdr }}/>
-        </section>
-    );
+      <div
+        className="episode-description markdown"
+        dangerouslySetInnerHTML={{ __html: mdr }}
+      />
+    </section>
+  );
 };
 
 Episode.propTypes = {
-    item: PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        title: PropTypes.string,
-        text: PropTypes.string,
-        duration: PropTypes.string,
-        file_mp3: PropTypes.string,
-        audio_type: PropTypes.string,
-        audio_size: PropTypes.number,
-    }).isRequired,
-    slug: PropTypes.string.isRequired,
-    iTunesURL: PropTypes.string,
-    feed: PropTypes.string.isRequired,
-    prev: PropTypes.string,
-    next: PropTypes.string,
+  item: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string,
+    text: PropTypes.string,
+    duration: PropTypes.string,
+    file_mp3: PropTypes.string,
+    audio_type: PropTypes.string,
+    audio_size: PropTypes.number,
+  }).isRequired,
+  slug: PropTypes.string.isRequired,
+  iTunesURL: PropTypes.string,
+  feed: PropTypes.string.isRequired,
+  prev: PropTypes.string,
+  next: PropTypes.string,
 };
 export default Episode;
--- src/js/components/header.jsx
+++ src/js/components/header.jsx
@@ -3,32 +3,68 @@ import { Link, NavLink } from 'react-router-dom';
 import { Sticky } from 'react-sticky';
 
 class Header extends React.Component {
-    render() {
-        return (
-            <header id="header">
-                <div className="wrapper">
-                    <div className="title">
-                        <Link to="/"><img className="logo" src="/img/logo.svg" alt="arlefreak"/></Link>
-                        <h2>by Mario Carballo<span className="hideMobile"> Zama</span></h2>
-                    </div>
-                    <Sticky stickyClassName="sticky">
-                        <nav>
-                            <ul>
-                                <li><NavLink exact to="/" activeClassName="active"><img src="/img/routes/home.svg" alt="home"/></NavLink></li>
-                                <li><NavLink to="/portfolio" activeClassName="active">Portfolio</NavLink></li>
-                                <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
-                                <li><NavLink to="/logs" activeClassName="active">Logs</NavLink></li>
-                                <li><NavLink to="/cv/en-games" activeClassName="active">CV</NavLink></li>
-                                <li><NavLink to="/subscribe" activeClassName="active"><img src="/img/routes/subscribe.svg" alt="subscribe"/></NavLink></li>
-                                <li><NavLink to="/ligo" activeClassName="active"><img src="/img/routes/ligoj.svg" alt="Φ"/></NavLink></li>
-                                <li><NavLink to="/podcasts/detras-del-pixel"><img src="/img/routes/btp.svg" alt="podcasts"/></NavLink></li>
-                            </ul>
-                        </nav>
-                    </Sticky>
-                </div>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header id="header">
+        <div className="wrapper">
+          <div className="title">
+            <Link to="/">
+              <img className="logo" src="/img/logo.svg" alt="arlefreak" />
+            </Link>
+            <h2>
+              by Mario Carballo<span className="hideMobile"> Zama</span>
+            </h2>
+          </div>
+          <Sticky stickyClassName="sticky">
+            <nav>
+              <ul>
+                <li>
+                  <NavLink exact to="/" activeClassName="active">
+                    <img src="/img/routes/home.svg" alt="home" />
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/portfolio" activeClassName="active">
+                    Portfolio
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/about" activeClassName="active">
+                    About
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/logs" activeClassName="active">
+                    Logs
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/cv/en-games" activeClassName="active">
+                    CV
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/subscribe" activeClassName="active">
+                    <img src="/img/routes/subscribe.svg" alt="subscribe" />
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/ligo" activeClassName="active">
+                    <img src="/img/routes/ligo.svg" alt="Φ" />
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/podcasts/detras-del-pixel">
+                    <img src="/img/routes/btp.svg" alt="podcasts" />
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </Sticky>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Header;
